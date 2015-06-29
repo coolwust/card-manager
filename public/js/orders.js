9f14899625c265179b4cd97b1977eca4e70f97b4
coolwust <coolwust@gmail.com>
2015-06-29 12:21:40 -0400

    5th commit

--- public/js/orders.js
+++ public/js/orders.js
@@ -18,8 +18,9 @@ document.addEventListener('DOMContentLoaded', function () {
   angular.bootstrap(OrdersComponent);
 });
 
-function OrdersFilterComponent() {
-  this.barFilters = [
+function FilterBar() {
+  this.bag = {};
+  this.bag.filters = [
     'Any',
     'Date Starting',
     'Date Ending',
@@ -31,56 +32,109 @@ function OrdersFilterComponent() {
     'L-Card Number',
     'B-Card Number'
   ];
-  this.barFilter = 'Any';
-  this.barDisabled = true;
-  this.barPlaceholder = '';
-  this.init = false;
+  this.bag.filter = 'Any';
+  this.bag.disabled = true;
+  this.bag.placeholder = '';
+  this.bag.init = false;
+}
+
+function FilterActive() {
+  this.bag = {};
+  this.bag.buttons = [
+    'Active',
+    'Legacy'
+  ];
+  this.bag.button = 'Active';
+}
+
+function FilterShipped() {
+  this.bag = {};
+  this.bag.buttons = [
+    'All',
+    'Preparing',
+    'Shipped'
+  ];
+  this.bag.button = 'All';
+}
+
+function FilterNormal() {
+  this.bag = {};
+  this.bag.buttons = [
+    'All',
+    'Narmal',
+    'Error'
+  ];
+  this.bag.button = 'All';
+}
+
+function OrdersFilterComponent(bar, active, shipped, normal) {
+  this.bar = bar.bag;
+  this.active = active.bag;
+  this.shipped = shipped.bag;
+  this.normal = normal.bag;
   //var socket = io('192.168.56.102:3000');
   //socket.emit('login');
 }
 
-OrdersFilterComponent.prototype.onSelectFilter = function (filter) {
-  document.getElementById('filterBar').value = '';
-  this.barFilter = filter;
-  this.init = false;
-  switch (filter) {
-    case 'Date Starting':
-    case 'Date Ending':
-    case 'Date Ordering':
-      this.barPlaceholder = 'MM-DD-YY';
-      this.barDisabled = false;
-      break;
-    case 'Any':
-      this.barDisabled = true;
-      this.barPlaceholder = '';
-      break;
-    default:
-    this.barDisabled = false;
-      this.barPlaceholder = '';
-  }
-}
-OrdersFilterComponent.prototype.onBarKeyUp = function ($event) {
-  this.init = true;
-  var value = $event.target.value;
-  switch (this.barFilter) {
-    case 'Date Starting':
-    case 'Date Ending':
-    case 'Date Ordering':
-      var regexp = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-      this.barValid = regexp.test(value);
-      break;
-    default:
-      this.barValid = (value.length > 0);
-      
-  }
-}
-
 OrdersFilterComponent.annotations = [
   new angular.ComponentAnnotation({
-    selector: 'orders-filter'
+    selector: 'orders-filter',
+    appInjector: [FilterBar, FilterActive, FilterShipped, FilterNormal]
   }),
   new angular.ViewAnnotation({
     templateUrl: '../tp/orders-filter.html',
     directives: [angular.NgFor, angular.NgIf]
   })
 ];
+
+OrdersFilterComponent.parameters = [
+  [FilterBar], [FilterActive], [FilterShipped], [FilterNormal]
+];
+
+OrdersFilterComponent.prototype.onSelectFilter = function (filter) {
+  document.getElementById('filter-bar').value = '';
+  this.bar.filter = filter;
+  this.bar.init = false;
+  switch (filter) {
+    case 'Date Starting':
+    case 'Date Ending':
+    case 'Date Ordering':
+      this.bar.placeholder = 'MM-DD-YY';
+      this.bar.disabled = false;
+      break;
+    case 'Any':
+      this.bar.disabled = true;
+      this.bar.placeholder = '';
+      break;
+    default:
+    this.bar.disabled = false;
+      this.bar.placeholder = '';
+  }
+}
+
+OrdersFilterComponent.prototype.onBarKeyUp = function ($event) {
+  this.bar.init = true;
+  var value = $event.target.value;
+  switch (this.bar.filter) {
+    case 'Date Starting':
+    case 'Date Ending':
+    case 'Date Ordering':
+      var regexp = /^(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+      this.bar.valid = regexp.test(value);
+      break;
+    default:
+      this.bar.valid = (value.length > 0);
+  }
+}
+
+OrdersFilterComponent.prototype.onActiveMouseUp = function (button) {
+  this.active.button = button;
+}
+
+OrdersFilterComponent.prototype.onShippedMouseUp = function (button) {
+  this.shipped.button = button;
+}
+
+OrdersFilterComponent.prototype.onNormalMouseUp = function (button) {
+  this.normal.button = button;
+}
